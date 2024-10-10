3c3a097562965e9bafb6db69bb4de608151eff16
vyshnavienarala <118157775+vyshnavienarala@users.noreply.github.com>
2024-10-10 11:39:40 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "Jenkins-sg" {
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = "ami-0dee22c13ea7a9a67"
   instance_type          = "t2.medium"
   key_name               = "mumbai-mac"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
